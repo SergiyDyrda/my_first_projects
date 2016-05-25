061670a7b88d4bb2dc61a6fdec493be40cec9374
SergiyDyrda <vatapower12@hotmail.com>
2016-05-25 18:50:32 +0300

    Add files via upload

---
+++ Factory Method/male/MailFactory.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level37.lesson04.big01.male;
+
+import com.javarush.test.level37.lesson04.big01.AbstractFactory;
+import com.javarush.test.level37.lesson04.big01.Human;
+
+/**
+ * Created by Segiy on 26.03.2016.
+ */
+public class MailFactory implements AbstractFactory {
+
+    public Human getPerson(int age) {
+        if (age <= KidBoy.MAX_AGE)
+            return new KidBoy();
+
+        else if (age <= TeenBoy.MAX_AGE)
+            return  new TeenBoy();
+
+        else return new Man();
+    }
+}
